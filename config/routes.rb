252928e6fe7cb4601178a5c7320da2fad00de33d
Michael Smethurst <michael@smethur.st>
2019-12-07 23:14:33 +0000

    browse by fragment

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   
   get 'adoption-dates' => 'adoption_date#index', as: :adoption_date_list
   get 'adoption-dates/:adoption_date' => 'adoption_date#show', as: :adoption_date_show
+  
+  get 'standing-order-fragments' => 'standing_order_fragment#index', as: :standing_order_fragment_list
+  get 'standing-order-fragments/:standing_order_fragment' => 'standing_order_fragment#show', as: :standing_order_fragment_show
 
   # Example resource route with options:
   #   resources :products do
