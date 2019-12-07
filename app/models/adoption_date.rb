c849fcbe0130026e45b84f805440577719538e43
Michael Smethurst <michael@smethur.st>
2019-12-07 22:59:16 +0000

    added belong to fragment version

--- app/models/adoption_date.rb
+++ app/models/adoption_date.rb
@@ -1,5 +1,4 @@
 class AdoptionDate < ActiveRecord::Base
   
-  has_many :standing_order_fragment_versions, -> { order(current_number: :asc) }
-  has_many :adoptions, -> { order(standing_order_number_in_list: :asc, standing_order_letter_in_list: :asc, fragment_number_in_list: :asc) }
+  has_many :standing_order_fragment_versions, -> { order(standing_order_number_in_list: :asc, standing_order_letter_in_list: :asc, fragment_number_in_list: :asc) }
 end
--- app/models/standing_order_fragment.rb
+++ app/models/standing_order_fragment.rb
@@ -1,2 +1,4 @@
 class StandingOrderFragment < ActiveRecord::Base
+  
+  has_many :standing_order_fragment_versions, -> { order(standing_order_number_in_list: :asc, standing_order_letter_in_list: :asc, fragment_number_in_list: :asc) }
 end
