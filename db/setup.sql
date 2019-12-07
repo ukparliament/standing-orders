1bdd1697beec157bc108deef9df063c666b1bb72
Michael Smethurst <michael@smethur.st>
2019-12-07 23:16:13 +0000

    simplified schema

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,4 @@
 /* main schema */
-drop table if exists adoptions;
 drop table if exists standing_order_fragment_versions;
 drop table if exists standing_order_fragments;
 drop table if exists adoption_dates;
@@ -18,6 +17,10 @@ create table standing_order_fragment_versions (
 	text varchar(2000) not null,
 	adopted_on date not null,
 	current_number varchar(10) not null,
+	standing_order_number varchar(5),
+	standing_order_number_in_list int,
+	standing_order_letter_in_list char(1),
+	fragment_number_in_list int,
 	root_number int not null,
 	reference int not null,
 	year int not null,
@@ -25,17 +28,5 @@ create table standing_order_fragment_versions (
 	standing_order_fragment_id int,
 	constraint fk_adoption_date foreign key (adoption_date_id) references adoption_dates(id),
 	constraint fk_standing_order_fragment foreign key (standing_order_fragment_id) references standing_order_fragments(id),
-	primary key (id)
-);
-create table adoptions (
-	id serial,
-	adoption_date_id int not null,
-	standing_order_fragment_version_id int not null,
-	standing_order_number varchar(5) not null,
-	standing_order_number_in_list int,
-	standing_order_letter_in_list char(1),
-	fragment_number_in_list int not null,
-	constraint fk_adoption_adoption_date foreign key (adoption_date_id) references adoption_dates(id),
-	constraint fk_standing_order_fragment_version foreign key (standing_order_fragment_version_id) references standing_order_fragment_versions(id),
 	primary key (id)
 );
