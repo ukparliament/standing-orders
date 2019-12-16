c3ba1ab5f8447e4257460fb0dabee6dfa8b19488
Robert Brook <robertbrook@fastmail.fm>
2019-12-16 23:09:09 +0000

    readable date

--- app/models/adoption_date.rb
+++ app/models/adoption_date.rb
@@ -3,6 +3,11 @@ class AdoptionDate < ActiveRecord::Base
   has_many :standing_order_fragment_versions, -> { order(standing_order_number_in_list: :asc, standing_order_letter_in_list: :asc, fragment_number_in_list: :asc) }
   
   def display_date
-    self.date.strftime( '%-d %B %Y')
+  	day_format = ActiveSupport::Inflector.ordinalize(date.day)
+    self.date.strftime("#{day_format} %B %Y")
+    # self.date.strftime( '%-d %B %Y')
   end
+  
 end
+
+
