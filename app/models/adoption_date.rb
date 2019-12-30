68aee89e191808171d228387b903bc65d279c39c
Robert Brook <robertbrook@fastmail.fm>
2019-12-30 20:21:46 +0000

    css fiddling

--- app/models/adoption_date.rb
+++ app/models/adoption_date.rb
@@ -1,13 +1,8 @@
 class AdoptionDate < ActiveRecord::Base
-  
   has_many :standing_order_fragment_versions, -> { order(standing_order_number_in_list: :asc, standing_order_letter_in_list: :asc, fragment_number_in_list: :asc) }
-  
+
   def display_date
-  	day_format = ActiveSupport::Inflector.ordinalize(date.day)
-    self.date.strftime("#{day_format} %B %Y")
-    # self.date.strftime( '%-d %B %Y')
+    day_format = ActiveSupport::Inflector.ordinalize(date.day)
+    date.strftime("%Y &mdash; #{day_format} %B").html_safe
   end
-  
 end
-
-
