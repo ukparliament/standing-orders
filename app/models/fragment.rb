e29b2455f045305042bd12b7221b0b3c6eadd9e1
Michael Smethurst <michael@smethur.st>
2020-11-14 21:52:38 +0000

    gets fragment versions joins with adoptions to get date

--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,4 +1,6 @@
 class Fragment < ActiveRecord::Base
   
-  has_many :fragment_versions
+  def fragment_versions
+    FragmentVersion.all.select( 'fv.*, a.date' ).joins( 'as fv, adoptions as a').where( "fv.adoption_id = a.id and fv.fragment_id = ?", self ).order( 'a.date' )
+  end
 end
