f999b95dec40f7dd1197dae6199538adbc131f32
Michael Smethurst <michael@smethur.st>
2020-11-14 17:22:28 +0000

    new adoption index page

--- app/controllers/adoption_controller.rb
+++ app/controllers/adoption_controller.rb
@@ -1,5 +1,9 @@
 class AdoptionController < ApplicationController
   
+  def index
+    @adoptions = Adoption.all.order( 'ordinality asc' )
+  end
+  
   def show
     adoption = params[:adoption]
     @adoption = Adoption.find( adoption )
