b80b310bf7efbffc34221f82b6158a9b5d3774d5
Michael Smethurst <michael@smethur.st>
2020-11-14 16:58:37 +0000

    added fragment version list

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,3 +1,4 @@
+drop table if exists fragment_versions;
 drop table if exists adoptions;
 
 create table adoptions (
@@ -6,5 +7,17 @@ create table adoptions (
 	ordinality serial not null,
 	parlrules_identifier varchar(20) not null,
 	primary key (id)
-)
-	
+);
+create table fragment_versions (
+	id serial not null,
+	adoption_id int not null,
+	parlrules_identifier varchar(20) not null,
+	current_number varchar(20) not null,
+	root_number varchar(20) not null,
+	text text not null,
+	parlrules_article_identifier varchar(20) not null,
+	article_current_number varchar(20) not null,
+	article_root_number varchar(20) not null,
+	constraint fk_adoption foreign key (adoption_id) references adoptions(id),
+	primary key (id)
+);
