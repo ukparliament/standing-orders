bbfefb8714288b9d65e5b04e74ff373ce3c1809e
Michael Smethurst <michael@smethur.st>
2020-11-14 17:58:23 +0000

    added display number method

--- app/models/fragment_version.rb
+++ app/models/fragment_version.rb
@@ -1,4 +1,12 @@
 class FragmentVersion < ActiveRecord::Base
   
   belongs_to :adoption
+  
+  def display_number
+    display_number = ''
+    if self.current_number.split( '.' ).last == '01'
+      display_number = self.current_number.split( '.' ).first + '.'
+    end
+    display_number
+  end
 end
