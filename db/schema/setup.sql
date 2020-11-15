bb4e24b20c5dc9daf35ea68e9ace54116c7c3578
Michael Smethurst <michael@smethur.st>
2020-11-15 16:56:14 +0000

    added orders table

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -1,6 +1,7 @@
 drop table if exists fragment_versions;
 drop table if exists fragments;
 drop table if exists order_versions;
+drop table if exists orders;
 drop table if exists adoptions;
 
 create table adoptions (
@@ -30,13 +31,20 @@ create table fragment_versions (
 	constraint fk_fragment foreign key (fragment_id) references fragments(id),
 	primary key (id)
 );
+create table orders (
+	id serial not null,
+	parlrules_identifier varchar(20) not null,
+	primary key (id)
+);
 create table order_versions (
 	id serial not null,
 	adoption_id int not null,
+	order_id int,
 	parlrules_identifier varchar(20) not null,
 	current_number varchar(20) not null,
 	root_number varchar(20) not null,
 	text text not null,
 	constraint fk_adoption foreign key (adoption_id) references adoptions(id),
+	constraint fk_order foreign key (order_id) references orders(id),
 	primary key (id)
 );
