9d4f97535d2b2c332b95236fff717fc822fc5022
Michael Smethurst <michael@smethur.st>
2020-11-15 16:12:16 +0000

    added relation to orders

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,6 +1,7 @@
 class Adoption < ActiveRecord::Base
   
   has_many :fragment_versions, -> { order( parlrules_identifier: :asc )}
+  has_many :order_versions, -> { order( parlrules_identifier: :asc )}
 
   def display_date
     day_format = ActiveSupport::Inflector.ordinalize(date.day)
