ae00b7050b14fb32f1f703cfa6cd12f67ee47887
Michael Smethurst <michael@smethur.st>
2020-11-15 17:57:52 +0000

    added order version method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ActiveRecord::Base
+  
+  def order_versions
+    OrderVersion.all.select( 'ov.*, a.date' ).joins( 'as ov, adoptions as a').where( "ov.adoption_id = a.id and ov.order_id = ?", self ).order( 'a.date' )
+  end
 end
