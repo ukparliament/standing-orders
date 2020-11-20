d97031e6b2b479b238e46e05e5f8cc522634a07a
Michael Smethurst <michael@smethur.st>
2020-11-20 21:15:45 +0000

    added association to fragment versions: ]

--- app/models/order_version.rb
+++ app/models/order_version.rb
@@ -2,6 +2,7 @@ class OrderVersion < ActiveRecord::Base
   
   belongs_to :adoption
   belongs_to :order
+  has_many :fragment_versions, -> { order( parlrules_identifier: :asc )}
   
   def display_number
     self.current_number + '.'
