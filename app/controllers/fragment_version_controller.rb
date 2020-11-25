1222342d7448fce1af96daabb3f75e59a0ebf19e
Michael Smethurst <michael@smethur.st>
2020-11-25 17:18:57 +0000

    added basic list and show views

--- app/controllers/fragment_version_controller.rb
+++ app/controllers/fragment_version_controller.rb
@@ -1,2 +1,11 @@
 class FragmentVersionController < ApplicationController
+  
+  def index
+    @fragment_versions = FragmentVersion.all
+  end
+  
+  def show
+    fragment_version = params[:fragment_version]
+    @fragment_version = FragmentVersion.find( fragment_version )
+  end
 end
