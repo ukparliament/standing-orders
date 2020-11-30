90f5779b54f5dae881386ec3aa58195048c68353
Michael Smethurst <michael@smethur.st>
2020-11-30 19:19:11 +0000

    added display date method. related to fragment and order versions

--- app/models/revision_set.rb
+++ app/models/revision_set.rb
@@ -1,2 +1,10 @@
 class RevisionSet < ActiveRecord::Base
+  
+  has_many :fragment_versions, -> { order( parlrules_identifier: :asc )}
+  has_many :order_versions, -> { order( parlrules_identifier: :asc )}
+
+  def display_date
+    day_format = ActiveSupport::Inflector.ordinalize(date.day)
+    date.strftime("<span class='b'>%Y</span> &mdash; #{day_format} %B").html_safe
+  end
 end
