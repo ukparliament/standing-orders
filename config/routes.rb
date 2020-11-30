b136fe9092057ca14f7e02d70c4acb36f55b673e
Michael Smethurst <michael@smethur.st>
2020-11-30 20:10:07 +0000

    don't put underscores in urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   
   get '/' =>'home#index', as: :home
   
-  get 'revision_sets' => 'revision_set#index', as: :revision_set_list
-  get 'revision_sets/:revision_set' => 'revision_set#show', as: :revision_set_show
-  get 'revision_sets/:revision_set/orders' => 'revision_set#order', as: :revision_set_order
-  get 'revision_sets/:revision_set/fragments' => 'revision_set#fragment', as: :revision_set_fragment
+  get 'revision-sets' => 'revision_set#index', as: :revision_set_list
+  get 'revision-sets/:revision_set' => 'revision_set#show', as: :revision_set_show
+  get 'revision-sets/:revision_set/orders' => 'revision_set#order', as: :revision_set_order
+  get 'revision-sets/:revision_set/fragments' => 'revision_set#fragment', as: :revision_set_fragment
   
   get 'fragments' => 'fragment#index', as: :fragment_list
   get 'fragments/:fragment' => 'fragment#show', as: :fragment_show
