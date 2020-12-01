4403e5788f8ff49a1623c2cf43f7a3beb47ed5f2
Michael Smethurst <michael@smethur.st>
2020-12-01 11:42:22 +0000

    swapped out adoption for revision set

--- app/controllers/revision_set_controller.rb
+++ app/controllers/revision_set_controller.rb
@@ -1,11 +1,21 @@
 class RevisionSetController < ApplicationController
   
   def index
-    @adoption_dates = AdoptionDate.all.order( 'date asc' )
+    @revision_sets = RevisionSet.all.order( 'date asc' )
   end
   
   def show
-    adoption_date = params[:adoption_date]
-    @adoption_date = AdoptionDate.find( adoption_date )
+    revision_set = params[:revision_set]
+    @revision_set = RevisionSet.find( revision_set )
+  end
+  
+  def fragment
+    revision_set = params[:revision_set]
+    @revision_set = RevisionSet.find( revision_set )
+  end
+  
+  def order
+    revision_set = params[:revision_set]
+    @revision_set = RevisionSet.find( revision_set )
   end
 end
