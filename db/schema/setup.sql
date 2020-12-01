7d85a806d00dbf80887dab4829b3ec85936b335f
Michael Smethurst <michael@smethur.st>
2020-12-01 18:47:04 +0000

    added new tables for houses and business_extents, populated them, added foreign keys from revision sets and orders

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -3,17 +3,40 @@ drop table if exists fragments;
 drop table if exists order_versions;
 drop table if exists orders;
 drop table if exists revision_sets;
+drop table if exists business_extents;
+drop table if exists houses;
 
+
+create table houses (
+	id serial not null,
+	name varchar(20) not null,
+	primary key (id)
+);
+insert into houses (name) values ('House of Commons');
+create table business_extents (
+	id serial not null,
+	label varchar(20) not null,
+	primary key (id)
+);
+insert into business_extents (label) values ('Public business');
 create table revision_sets (
 	id serial not null,
 	date date not null,
 	ordinality serial not null,
 	parlrules_identifier varchar(20) not null,
+	business_extent_id int not null,
+	house_id int not null,
+	constraint fk_business_extent foreign key (business_extent_id) references business_extents(id),
+	constraint fk_house foreign key (house_id) references houses(id),
 	primary key (id)
 );
 create table orders (
 	id serial not null,
 	parlrules_identifier varchar(20) not null,
+	business_extent_id int not null,
+	house_id int not null,
+	constraint fk_business_extent foreign key (business_extent_id) references business_extents(id),
+	constraint fk_house foreign key (house_id) references houses(id),
 	primary key (id)
 );
 create table order_versions (
