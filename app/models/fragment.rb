aef0777d7340f7f860773e38f6e0c0b139080f5a
Michael Smethurst <michael@smethur.st>
2020-12-01 11:40:58 +0000

    query to get versions of fragment now uses revision set not adoption

--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,6 +1,6 @@
 class Fragment < ActiveRecord::Base
   
   def fragment_versions
-    FragmentVersion.all.select( 'fv.*, a.date' ).joins( 'as fv, adoptions as a').where( "fv.adoption_id = a.id and fv.fragment_id = ?", self ).order( 'a.date' )
+    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs').where( "fv.revision_set_id = rs.id and fv.fragment_id = ?", self ).order( 'rs.date' )
   end
 end
