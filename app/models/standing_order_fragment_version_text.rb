d5eaa4bf57dbf77e8d67d31cc4ef9d66bb3daa8c
Michael Smethurst <michael@smethur.st>
2020-12-01 11:51:27 +0000

    removed standing order fragment version text  model

--- app/models/standing_order_fragment_version_text.rb
+++
@@ -1,7 +0,0 @@
-class StandingOrderFragmentVersionText < ActiveRecord::Base
-
-	def show
-  		@text = @standing_order_fragment_version_text.text?
-	end
-	
-end
--- test/models/standing_order_fragment_version_text_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StandingOrderFragmentVersionTextTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
