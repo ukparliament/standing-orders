964178ccb6849f1010376046a28bbd0950b97797
Michael Smethurst <michael@smethur.st>
2020-12-01 18:45:18 +0000

    added relationships to house and business extent

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ActiveRecord::Base
   
+  belongs_to :house
+  belongs_to :business_extent
+  
   def order_versions
     OrderVersion.all.select( 'ov.*, rs.date' ).joins( 'as ov, revision_sets as rs').where( "ov.revision_set_id = rs.id and ov.order_id = ?", self ).order( 'rs.date' )
   end
