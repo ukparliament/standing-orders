533131d839d37d8d4b1b4cdd9e49dd814797cb95
Michael Smethurst <michael@smethur.st>
2020-12-01 18:46:22 +0000

    not a clue

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,10 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "business_extents", force: :cascade do |t|
+    t.string "label", limit: 20, null: false
+  end
+
   create_table "fragment_versions", force: :cascade do |t|
     t.integer "revision_set_id",                         null: false
     t.integer "fragment_id"
@@ -34,6 +38,10 @@ ActiveRecord::Schema.define(version: 0) do
     t.string "parlrules_identifier", limit: 20, null: false
   end
 
+  create_table "houses", force: :cascade do |t|
+    t.string "name", limit: 20, null: false
+  end
+
   create_table "order_versions", force: :cascade do |t|
     t.integer "revision_set_id",                 null: false
     t.integer "order_id"
@@ -43,13 +51,17 @@ ActiveRecord::Schema.define(version: 0) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "parlrules_identifier", limit: 20, null: false
+    t.string  "parlrules_identifier", limit: 20, null: false
+    t.integer "business_extent_id",              null: false
+    t.integer "house_id",                        null: false
   end
 
   create_table "revision_sets", force: :cascade do |t|
     t.date    "date",                                                                                          null: false
     t.integer "ordinality",                      default: "nextval('revision_sets_ordinality_seq'::regclass)", null: false
     t.string  "parlrules_identifier", limit: 20,                                                               null: false
+    t.integer "business_extent_id",                                                                            null: false
+    t.integer "house_id",                                                                                      null: false
   end
 
   add_foreign_key "fragment_versions", "fragments", name: "fk_fragment"
@@ -58,4 +70,8 @@ ActiveRecord::Schema.define(version: 0) do
   add_foreign_key "fragment_versions", "revision_sets", name: "fk_revision_set"
   add_foreign_key "order_versions", "orders", name: "fk_order"
   add_foreign_key "order_versions", "revision_sets", name: "fk_revision_set"
+  add_foreign_key "orders", "business_extents", name: "fk_business_extent"
+  add_foreign_key "orders", "houses", name: "fk_house"
+  add_foreign_key "revision_sets", "business_extents", name: "fk_business_extent"
+  add_foreign_key "revision_sets", "houses", name: "fk_house"
 end
