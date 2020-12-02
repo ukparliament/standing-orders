d9576a6a3075a027282ea7e3b245ff3137f930e9
Michael Smethurst <michael@smethur.st>
2020-12-02 10:56:00 +0000

    removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,28 +29,7 @@ Rails.application.routes.draw do
   get 'fragment-versions' => 'fragment_version#index', as: :fragment_version_list
   get 'fragment-versions/:fragment_version' => 'fragment_version#show', as: :fragment_version_show
   
-  
-  
-  
-  
-  
-  
-  
-  
-  get 'adoption-dates' => 'adoption_date#index', as: :adoption_date_list
-  
-  get 'adoption-dates/:adoption_date' => 'adoption_date#show', as: :adoption_date_show
-  
-  get 'standing-order-fragments' => 'standing_order_fragment#index', as: :standing_order_fragment_list
-  
-  get 'standing-order-fragments/:standing_order_fragment' => 'standing_order_fragment#show', as: :standing_order_fragment_show
-  
-  get 'sankey' => 'sankey#show', as: :sankey_show
-  
   get 'meta' => 'meta#index', as: :meta_list
-  
   get 'meta/schema' => 'meta#schema', as: :meta_schema
-  
   get 'meta/dump' => 'meta#dump', as: :meta_dump
-
 end
