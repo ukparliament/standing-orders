be55b90ab161d095b0adb2a5463fd33c3a34598d
Michael Smethurst <michael@smethur.st>
2020-12-02 16:13:40 +0000

    added methods for from fragment version and to fragment version

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,2 +1,10 @@
 class Revision < ActiveRecord::Base
+  
+  def from_fragment_version
+    FragmentVersion.all.where( 'id =?', self.from_fragment_version_id ).first
+  end
+  
+  def to_fragment_version
+    FragmentVersion.all.where( 'id =?', self.to_fragment_version_id ).first
+  end
 end
