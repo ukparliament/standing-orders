1c15f8a140f4be17b09099eccba756faeb1c8ac1
Michael Smethurst <michael@smethur.st>
2020-12-02 12:30:00 +0000

    added citation in list method using first fragment identifier. might be better done differently

--- app/models/order_version.rb
+++ app/models/order_version.rb
@@ -4,6 +4,23 @@ class OrderVersion < ActiveRecord::Base
   belongs_to :order
   has_many :fragment_versions, -> { order( parlrules_identifier: :asc )}
   
+  def citation_in_list
+    citation_in_list = self.fragment_versions.first.parlrules_identifier
+    # swap . for -
+    citation_in_list = citation_in_list.gsub( '.', '-' )
+    # remove any zeros in tenths column
+    citation_in_list = citation_in_list.gsub( '-0', '-' )
+    # remove any zeros in units column
+    citation_in_list = citation_in_list.gsub( '0-', '-' )
+    # remove leading 0s
+    while ( citation_in_list[0,1] == '0' )
+      citation_in_list = citation_in_list[1, citation_in_list.size]
+    end
+    # Remove -1 if it's the first fragment in an order
+    citation_in_list = citation_in_list[0, citation_in_list.size - 2] if citation_in_list[citation_in_list.size - 2, 2] == '-1'
+    citation_in_list
+  end
+  
   def display_number
     self.current_number + '.'
   end
