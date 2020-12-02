4e7b916ce5d893679c70fe90972811e870388603
Michael Smethurst <michael@smethur.st>
2020-12-02 16:48:10 +0000

    added method to return level: major / minor

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -7,4 +7,10 @@ class Revision < ActiveRecord::Base
   def to_fragment_version
     FragmentVersion.all.where( 'id =?', self.to_fragment_version_id ).first
   end
+  
+  def level
+    level = 'Minor'
+    level = 'Major' if self.is_major
+    level
+  end
 end
