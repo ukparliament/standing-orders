cf92f39dd42574e1a63ce4a5fe4abbba34bb4f7a
Michael Smethurst <michael@smethur.st>
2020-12-03 20:55:56 +0000

    added actions for versions of an order and versions of an order with revisions

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -8,4 +8,24 @@ class OrderController < ApplicationController
     order = params[:order]
     @order = Order.find( order )
   end
+  
+  def versions
+    order = params[:order]
+    @order = Order.find( order )
+  end
+  
+  def revisions
+    order = params[:order]
+    @order = Order.find( order )
+  end
+  
+  def minor_revisions
+    order = params[:order]
+    @order = Order.find( order )
+  end
+  
+  def major_revisions
+    order = params[:order]
+    @order = Order.find( order )
+  end
 end
