260a97d68b032ac7183d47beee3e874ac848af59
Michael Smethurst <michael@smethur.st>
2020-12-03 20:55:17 +0000

    added urls for versions and versions with revisions for an order

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
   
   get 'orders' => 'order#index', as: :order_list
   get 'orders/:order' => 'order#show', as: :order_show
+  get 'orders/:order/versions' => 'order#versions', as: :order_versions
+  get 'orders/:order/versions/revisions' => 'order#revisions', as: :order_revisions
+  get 'orders/:order/versions/revisions/minor' => 'order#minor_revisions', as: :order_minor_revisions
+  get 'orders/:order/versions/revisions/major' => 'order#major_revisions', as: :order_major_revisions
   
   get 'order-versions' => 'order_version#index', as: :order_version_list
   get 'order-versions/:order_version' => 'order_version#show', as: :order_version_show
