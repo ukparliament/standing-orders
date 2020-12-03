9e1db6b38aa40a25023e6e33008eb7e30238ebaa
Michael Smethurst <michael@smethur.st>
2020-12-03 13:23:32 +0000

    new menthods to list fragment versions with revisions

--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,7 +1,19 @@
 class Fragment < ActiveRecord::Base
   
   def fragment_versions
-    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs').where( "fv.revision_set_id = rs.id and fv.fragment_id = ?", self ).order( 'rs.date' )
+    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs').where( "fv.revision_set_id = rs.id and fv.fragment_id = ?", self ).order( 'rs.ordinality' )
+  end
+  
+  def fragment_versions_with_revisions
+    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs, revisions as r' ).where( "fv.revision_set_id = rs.id and fv.fragment_id = ? and fv.id = r.from_fragment_version_id", self ).order( 'rs.ordinality' )
+  end
+  
+  def fragment_versions_with_minor_revisions
+    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs, revisions as r' ).where( "fv.revision_set_id = rs.id and fv.fragment_id = ? and fv.id = r.from_fragment_version_id and r.is_major is false", self ).order( 'rs.ordinality' )
+  end
+  
+  def fragment_versions_with_major_revisions
+    FragmentVersion.all.select( 'fv.*, rs.date' ).joins( 'as fv, revision_sets as rs, revisions as r' ).where( "fv.revision_set_id = rs.id and fv.fragment_id = ? and fv.id = r.from_fragment_version_id and r.is_major is true", self ).order( 'rs.ordinality' )
   end
   
   def display_title
