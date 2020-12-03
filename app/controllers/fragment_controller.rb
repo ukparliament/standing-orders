90fdf17729c2fd14d32f961012cc77dfb671db42
Michael Smethurst <michael@smethur.st>
2020-12-03 13:04:35 +0000

    new actions to display versions and revisions

--- app/controllers/fragment_controller.rb
+++ app/controllers/fragment_controller.rb
@@ -8,4 +8,24 @@ class FragmentController < ApplicationController
     fragment = params[:fragment]
     @fragment = Fragment.find( fragment )
   end
+  
+  def versions
+    fragment = params[:fragment]
+    @fragment = Fragment.find( fragment )
+  end
+  
+  def revisions
+    fragment = params[:fragment]
+    @fragment = Fragment.find( fragment )
+  end
+  
+  def minor_revisions
+    fragment = params[:fragment]
+    @fragment = Fragment.find( fragment )
+  end
+  
+  def major_revisions
+    fragment = params[:fragment]
+    @fragment = Fragment.find( fragment )
+  end
 end
