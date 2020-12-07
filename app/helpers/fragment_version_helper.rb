29c24be442b9d6308c709ee330561586cc2d4b23
Michael Smethurst <michael@smethur.st>
2020-12-07 17:19:45 +0000

    removed display order link method

--- app/helpers/fragment_version_helper.rb
+++ app/helpers/fragment_version_helper.rb
@@ -1,10 +1,2 @@
 module FragmentVersionHelper
-  
-  def display_order_link( fragment_version )
-    display_number = ''
-    if fragment_version.current_number.split( '.' ).last == '01'
-      display_number = link_to( fragment_version.current_number.split( '.' ).first + '.', order_show_url( :order => fragment_version.order ) )
-    end
-    display_number
-  end
 end
