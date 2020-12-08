c3c28d8306528797e81de4861cc9726bc3b3151f
Michael Smethurst <michael@smethur.st>
2020-12-08 16:21:00 +0000

    replaced ordinality based on parlrule_identifier with ordinality

--- app/models/revision_set.rb
+++ app/models/revision_set.rb
@@ -2,8 +2,8 @@ class RevisionSet < ActiveRecord::Base
   
   belongs_to :house
   belongs_to :business_extent
-  has_many :fragment_versions, -> { order( parlrules_identifier: :asc )}
-  has_many :order_versions, -> { order( parlrules_identifier: :asc )}
+  has_many :fragment_versions, -> { order( ordinality: :asc )}
+  has_many :order_versions, -> { order( ordinality: :asc )}
 
   def display_date
     day_format = ActiveSupport::Inflector.ordinalize(date.day)
