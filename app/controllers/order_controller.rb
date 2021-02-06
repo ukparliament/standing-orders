7da1b9625b05172210662ca1828572134df6ba1c
Michael Smethurst <michael@smethur.st>
2021-02-06 19:02:25 +0000

    added redirect method

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -9,6 +9,17 @@ class OrderController < ApplicationController
     @order = Order.find( order )
   end
   
+  def redirect
+    order = params[:order]
+    @order = Order.find( order )
+    if @order.remains_in_operation?
+      order_version = @order.order_versions.last
+      redirect_to( revision_set_show_url( :revision_set => order_version.revision_set, :anchor => "order-#{order_version.citation_in_list}" ) )
+    else
+      redirect_to( order_show_url )
+    end
+  end
+  
   def versions
     order = params[:order]
     @order = Order.find( order )
