33cb11be559e7b61415c0a005d0f5828a4de381d
Michael Smethurst <michael@smethur.st>
2022-06-23 13:24:47 +0100

    removed associations to house and business extents

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   
-  belongs_to :house
-  belongs_to :business_extent
+  #belongs_to :house
+  #belongs_to :business_extent
   
   def order_versions
     OrderVersion.all.select( 'ov.*, rs.date' ).joins( 'as ov, revision_sets as rs').where( "ov.revision_set_id = rs.id and ov.order_id = ?", self ).order( 'rs.date' )
@@ -20,7 +20,8 @@ class Order < ActiveRecord::Base
   end
   
   def display_title
-    display_title = self.house.name + ' &mdash; ' + self.business_extent.label
+    display_title = ''
+    #display_title = self.house.name + ' &mdash; ' + self.business_extent.label
     display_title = display_title + ' &mdash; Order ID:' + self.id.to_s
     display_title.html_safe
   end
